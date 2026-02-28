71b24ce2a4240ddc95c66437ac35bca64a34fe76
VS-Studio-Hub <venthanf@gmail.com>
2026-02-27 20:17:49 -0500

    UpdatedVersion

---
+++ Assets/Scripts/BlackholeAnim.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class BlackholeAnim : MonoBehaviour
+{
+    private Transform transform1;
+    public Vector3 newScale;
+    private Material material;
+    float a = 1f;
+    float b = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (transform1 == null)
+        {
+            transform1 = GetComponent<Transform>();
+
+        }
+        newScale = this.transform1.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, 100 * Time.deltaTime, 0, Space.Self);
+        newScale = new Vector3(a, a, a);
+        if (newScale.x == 1)
+        {
+            
+        }
+
+        if (!(newScale.z == this.transform.localScale.z))
+        {
+            Vector3 Addition = new Vector3(0.05f, 0.05f, 0.05f);
+            this.transform.localScale += Addition * Time.deltaTime * b;
+        }
+    }
+}
